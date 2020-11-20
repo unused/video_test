0469b22ea4a32bf6058a65a3ac9b16536364c036
Markus <markus.andreas@web.wien>
2020-11-20 15:31:59 +0100

    first commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,28 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import eyeson, { StreamHelpers } from 'eyeson';
+
+
+eyeson.onEvent(event => {
+    if (event.type !== "accept") {
+        return;
+    }
+    // Note: Some iOS devices might require video to have autoplay attribute set.
+    let video = document.querySelector("video");
+    video.srcObject = event.remoteStream;
+    video.play();
+});
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    console.log("In");
+    console.log(document.querySelector("video").dataset.key);
+    eyeson.start(document.querySelector("video").dataset.key);
+});
